c7db00cdddf0df06dc469f15e2dc68fd8f8d1c5f
emredersiniz <112582013+emre-cavus@users.noreply.github.com>
2023-03-08 22:40:31 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function updateDisplay(){
 
 keys.addEventListener('click' ,function(e){
     const element = e.target;
-    if(element.matches('button')) return;
+    if(!element.matches('button')) return;
 
     if(element.classList.contains('operator')){
         console.log('operator', element.value);
